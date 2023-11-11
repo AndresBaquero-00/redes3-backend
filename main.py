f71bdfaaec519b40de5a633c3a496e70c13d044b
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 16:40:32 -0500

    Añadida variable de entorno de mongo y realizada inserción de documentos.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
+import pymongo
 from flask import request, jsonify, Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
+
+client = pymongo.MongoClient(os.getenv('MONGODB_HOST'))
+db  = client['sensors_data']
+col = db['data']
 
 @app.route('/')
 def home():
@@ -11,10 +19,9 @@ def home():
 
 @app.route('/api/v2/save', methods=['POST'])
 def save_data():
-    print(request.method)
     data = request.get_json()
-    print(data)
-    return jsonify({'ok': True, 'status': 200, 'message': 'Hello World!'})
+    res = col.insert_one(data)
+    return jsonify({'ok': True, 'status': 200, 'message': res.inserted_id})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
