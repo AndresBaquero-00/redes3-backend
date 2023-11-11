1c372af465d252ee988ab67fd15e76a00ddecfb2
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 17:08:00 -0500

    Añadido endpoint para recibir la información de manera encriptada.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import json
 import os
 import pymongo
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
 from flask import request, jsonify, Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -13,6 +16,8 @@ client = pymongo.MongoClient(os.getenv('MONGODB_HOST'))
 db  = client['sensors_data']
 col = db['data']
 
+cipher = AES.new(bytes.fromhex(os.getenv('CRYPTO_KEY')), AES.MODE_ECB)
+
 @app.route('/')
 def home():
     return jsonify({'ok': True, 'status': 200, 'message': 'Hello World!'})
@@ -21,6 +26,15 @@ def home():
 def save_data():
     data = request.get_json()
     res = col.insert_one(data)
+    return jsonify({'ok': True, 'status': 200, 'message': str(res.inserted_id)})
+
+@app.route('/api/v2/save-secure', methods=['POST'])
+def save_data():
+    data = request.get_data().decode()
+    print(data)
+    raw = Padding.unpad(cipher.decrypt(bytes.fromhex(data)), 16).decode()
+    print(raw)
+    res = col.insert_one(json.loads(raw))
     return jsonify({'ok': True, 'status': 200, 'message': str(res.inserted_id)})
 
 if __name__ == '__main__':
