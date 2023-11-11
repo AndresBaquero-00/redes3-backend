5698b5d287bd417fd83cfcecf44a85156b75c9b0
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 16:30:48 -0500

    Especificado POST como método.

--- main.py
+++ main.py
@@ -9,15 +9,12 @@ CORS(app)
 def home():
     return jsonify({'ok': True, 'status': 200, 'message': 'Hello World!'})
 
-@app.route('/api/v2/save')
+@app.route('/api/v2/save', methods=['POST'])
 def save_data():
     print(request.method)
-    if request.method == 'POST':
-        data = request.get_json()
-        print(data)
-        return jsonify({'ok': True, 'status': 200, 'message': 'Hello World!'})
-    
-    return jsonify({'ok': False, 'status': 400})
+    data = request.get_json()
+    print(data)
+    return jsonify({'ok': True, 'status': 200, 'message': 'Hello World!'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
