37f2b983e9dab92663051b995022a063b3ea710c
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 18:07:14 -0500

    Revisando envío de data.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def save_data_secure():
 
 @app.route('/api/v2/list', methods=['GET'])
 def list_data():
-    data = col.find().sort({'$datetime': -1}).limit(10)
+    data = col.find().sort({'datetime': -1}).limit(10)
     for d in list(data):
         print(d)
     return jsonify({'ok': True, 'status': 200, 'message': 'Operación realizada exitosamente.'})
