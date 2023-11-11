68a213674a30c6c3a9e54b9660684cdd33d74014
AndresBaquero-00 <andres.baquero6@gmail.com>
2023-11-11 16:28:58 -0500

    Añadido print de la request y retornando respuesta de error.

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ def home():
 
 @app.route('/api/v2/save')
 def save_data():
+    print(request.method)
     if request.method == 'POST':
         data = request.get_json()
         print(data)
         return jsonify({'ok': True, 'status': 200, 'message': 'Hello World!'})
+    
+    return jsonify({'ok': False, 'status': 400})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
